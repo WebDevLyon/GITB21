0e8efb6396323e02a7c6924d8ef5d12af95406ae
Nicolas Lachise <webdeveloppeurlyon@gmail.com>
2020-07-28 22:58:30 +0200

    Refonte design appli
    Refonte du design
    Suppression des fichiers inutiles

--- front/src/components/App.vue
+++ front/src/components/App.vue
@@ -1,37 +1,32 @@
 <template>
-  <div id="app">
-    <Header />
-    <router-view />
+  <div id="app" class="page-content">
+    <Nav />
+    <div class="view">
+      <SearchBar />
+      <router-view class="view__session" />
+    </div>
   </div>
 </template>
+
 <script>
-import Header from "@/components/Header";
+import Nav from "@/components/Nav";
+import SearchBar from "@/components/SearchBar";
+
 export default {
   name: "App",
   components: {
-    Header,
+    Nav,
+    SearchBar
   },
 };
-</script>script
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.view{
+  display: flex;
+  flex-direction: column;
+  &__session{
+    background : $BG-content;
+    flex: 1;
   }
 }
 </style>
---
+++ front/src/components/SearchBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="searchBar">
+    <div
+      class="searchBar__input-block"
+    >Lorem ipsum dolor sit amet consectetur adipisicing elit. At, repudiandae?</div>
+    <div>
+      <i>LINK DISCONNECT</i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+};
+</script>
+
+<style lang='scss'>
+.searchBar {
+  display: flex;
+  justify-content: space-between;
+  background: $BG-searchbar;
+  padding: 1rem 1rem;
+  min-height: 1rem;
+}
+</style>
