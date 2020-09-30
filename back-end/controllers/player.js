5beca16a32521719e8cdceaddbbcef089f4b0691
Nicolas Lachise <webdeveloppeurlyon@gmail.com>
2020-09-30 16:59:34 +0200

    Interrupteur inscription
    Fin design tablePlayer
    Reste a faire délocaliser l’interrupteur en composant

--- back-end/controllers/player.js
+++ back-end/controllers/player.js
@@ -1,55 +1,46 @@
-const Player = require('../models/Player');
-const User = require('../models/User');
-const Association = require('../models/Association');
-
+const Player = require("../models/Player");
+const User = require("../models/User");
+const Association = require("../models/Association");
 
 exports.add = (req, res, next) => {
-    /*
+  /*
     TODO: Vérification de la requête par regex
     */
-    /*
+  /*
      TODO: Sauver le player dans la DB
      */
-    const player = new Player({
-        name: req.body.nom,
-        prenom: req.body.prenom,
-        mail: req.body.nom.email,
-        tel: req.body.tel,
-        association: req.body.association,
-        optionTournois: req.body.tournoiOption,
-    })
-    //?vérification du joueur créé ?
-    console.log(player);
+  const player = new Player({
+    name: req.body.nom,
+    prenom: req.body.prenom,
+    mail: req.body.nom.email,
+    tel: req.body.tel,
+    association: req.body.association,
+    optionTournois: req.body.tournoiOption,
+  });
+  //?vérification du joueur créé ?
+  console.log(player);
 
-    //* Enregistrement dans la DB
-    player.save()
-        .then(() => res.status(201).json({ 'Création du joueur': player }))
-        .catch(err => res.status(400).json({ err }))
-    //    TODO: Renvoyer erreur ou confirmation de l'enregistrement
+  //* Enregistrement dans la DB
+  player
+    .save()
+    .then(() => res.status(201).json({ "Création du joueur": player }))
+    .catch((err) => res.status(400).json({ err }));
+  //    TODO: Renvoyer erreur ou confirmation de l'enregistrement
 };
 
 exports.list = (req, res, next) => {
-    console.log(req.query)
-    Player.find(req.query).populate('association')
-        .then(players => { res.status(200).json({ players }) })
-        .catch(err => res.status(400).json({ err }))
-}
-
+  console.log(req.query);
+  Player.find(req.query)
+    .populate("association")
+    .then((players) => {
+      res.status(200).json({ players });
+    })
+    .catch((err) => res.status(400).json({ err }));
+};
+/*A supprimer pour production
 exports.addBD = (req, res, next) => {
-    Player.find()
-        .then(players => {
-            Association.findOne({ _id: players[0].association })
-                .then(asso => {
-                    players.forEach(player => {
-                        asso.joueurs.push(player)
-                    })
-                    console.log('table joueurs', asso.joueurs)
-                    asso.save().then(() => res.status(201).json({ msg: 'reussi' }))
-                        .catch(err => res.status(400).json({ err }))
-
-                    next()
-                })
-
-        })
-        .catch(err => { console.log(err); next() })
-}
+  Player.updateMany({}, { inscription: true })
+    .then(() => res.status(203).json({ msg: "modif effectuée" }))
+    .catch((err) => res.status(500).json({ err }));
+};
+*/
--- back-end/models/Player.js
+++ back-end/models/Player.js
@@ -2,21 +2,22 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const playerSchema = mongoose.Schema({
-    nom: { type: String, required: true },
-    prenom: { type: String, required: true },
-    age: { type: Number, required: true },
-    mail: { type: String },
-    tel: { type: String },
-    entrainement: { type: Boolean, default: false },
-    competiteur: { type: Boolean, default: false },
-    interclub: { type: Boolean, default: false },
-    payement: { type: Number },
-    modePaiement: { type: String },
-    commentaires: { type: String },
-    association: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Association'
-    },
+  nom: { type: String, required: true },
+  prenom: { type: String, required: true },
+  age: { type: Number, required: true },
+  mail: { type: String },
+  tel: { type: String },
+  entrainement: { type: Boolean, default: false },
+  competiteur: { type: Boolean, default: false },
+  interclub: { type: Boolean, default: false },
+  payement: { type: Number },
+  modePaiement: { type: String },
+  commentaires: { type: String },
+  association: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Association",
+  },
+  inscription: { type: Boolean, default: false },
 });
 
 playerSchema.plugin(uniqueValidator);
