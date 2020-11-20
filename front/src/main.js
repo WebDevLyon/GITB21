78c6de106224248fbe7dcb7415b2ea0075acf43a
Nicolas Lachise <webdeveloppeurlyon@gmail.com>
2020-11-20 18:16:53 +0100

    Add Vuetify

--- front/src/main.js
+++ front/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import AuthWrapper from './components/AuthWrapper.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(AuthWrapper)
 }).$mount('#app')
--- front/vue.config.js
+++ front/vue.config.js
@@ -1,11 +1,4 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/production-sub-path/" : "/",
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: `@import "@/sass/_global.scss";`,
-      },
-    },
-  },
+  publicPath: "/",
+  transpileDependencies: ["vuetify"],
 };
